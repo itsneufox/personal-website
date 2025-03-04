62014752efb7f99f687ba6e574fb0b13123030d4
itsneufox <156133096+itsneufox@users.noreply.github.com>
2025-03-04 03:48:33 +0000

    Update GitHub Actions workflow for Next.js deployment and adjust configuration settings

--- next.config.js
+++ next.config.js
@@ -2,12 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  output: 'export',
   images: {
     unoptimized: true,
-  },
-  output: 'export',
+  }
 }
 
 module.exports = nextConfig
